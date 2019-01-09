5511287202eafac6214fa038b751618809a8e593
Four5Fire <942558046@qq.com>
2019-01-09 10:06:12 +0800

    完成了小程序登录授权,完成任务列表请求接口
    12.17完成文件上传接口和文件下载接口
    
    12.18整体功能完成,完成视频转码.修复视频下载路径缺失

---
+++ 小程序端/components/filelist/filelist.js
@@ -0,0 +1,26 @@
+// components/filelist/filelist.js
+import * as api from '../../static/api.js'
+import * as inter from '../../static/interface.js'
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
